da08e8e51b6b7edbcf8daf67f08025b75afb5383
seungeun.baek <dev.seungeun@gmail.com>
2021-09-25 23:32:43 +0900

    집 PC 인텔리제이 깃 연동 테스트

--- JavaProject/src/test/Hello.java
+++ JavaProject/src/test/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello, World");
 		System.out.println("I'm seungeun.baek");
+		System.out.println("Home Intellij Commit Test");
 	}
 }
