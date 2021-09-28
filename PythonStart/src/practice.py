844aed589c807d5300f0db9e9d9a7e5874479b02
seungeun.baek <dev.seungeun@gmail.com>
2021-09-29 01:15:35 +0900

    주석수정

--- PythonStart/src/practice.py
+++ PythonStart/src/practice.py
@@ -1,9 +1,4 @@
-"""
-변수선언
-주소값 확인
-값일치 확인
-"""
-
+# 변수선언
 a = 1
 b = 1.2
 c = b
@@ -11,8 +6,3 @@ c = b
 print(id(a), id(1), id(b), id(c))  # 주소값
 print(a is b, a == b)  # False False
 print(b is c, b == c)  # True True
-
-
-import sys
-sys.path.append("/home")
-print(sys.path)
