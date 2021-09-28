8004023de2fb39a4cde6822eae43befde0e55029
seungeun.baek <dev.seungeun@gmail.com>
2021-09-28 23:59:11 +0900

    주석 수정

--- PythonStart/test.py
+++ PythonStart/test.py
@@ -3,5 +3,5 @@ b = 1.2
 c = b
 
 print(id(a), id(1), id(b), id(c))  # 주소값
-print(a is b, a == b)  # false
-print(b is c, b == c)  # true
+print(a is b, a == b)  # False False
+print(b is c, b == c)  # True True
